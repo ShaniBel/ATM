fe4e85c379eba26c009543f3383cc4cedd07650a
Shani B <shanibelisha2@gmail.com>
2021-08-02 20:30:37 +0300

    adding files of working google map

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 import axios from "./node_modules/@bundled-es-modules/axios/axios.js";
 import { DataTable } from "./node_modules/simple-datatables/dist/module/index.js";
-console.log(DataTable);
+
 const response = axios
-  .get("https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=40")
+  .get("https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=500")
   .then((response) => {
-    response.data;
-    console.log(response);
     createTableHead(response.data.result.fields);
     createTableBody(response.data.result.records);
     const dataTable = new DataTable(myTable);
+    google.maps.event.addDomListener(window, "load", createGoogleMap(response.data.result.records));
   })
   .catch((err) => console.log(err));
 
@@ -29,7 +28,6 @@ const createTableHead = (data) => {
 };
 
 const createTableBody = (data) => {
-  console.log(data);
   for (const iterator of data) {
     let trEl = document.createElement("tr");
     for (const key in iterator) {
@@ -42,45 +40,43 @@ const createTableBody = (data) => {
   }
 };
 
-function init_map() {
-  let selectorMapElement = document.querySelector("#gmap_canvas");
-  let googleMapTitle = "ATM 1";
-  let googleMapAddress = "disingov 31 Tel-Aviv";
-  let googleMapLat = 31.4037193;
-  let googleMapLong = 33.9606947;
+function createGoogleMap(data) {
+  for (let i = 0; i < data.length; i++) {
+    init_map(data[i]);
+  }
+}
 
-  const myOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(googleMapLat, googleMapLong),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  };
+let selectorMapElement = document.querySelector("#gmap_canvas");
+
+const myOptions = {
+  zoom: 8,
+  center: new google.maps.LatLng(31.4037192, 33.9606743),
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+};
+
+let map = new google.maps.Map(selectorMapElement, myOptions);
+
+function init_map(dataATMObject) {
+  let googleMapTitle = `ATM ${dataATMObject.ATM_Address}`;
+  let googleMapAddress = `${dataATMObject.ATM_Address}, ${dataATMObject.City}`;
+  let googleMapLat = dataATMObject.Y_Coordinate;
+  let googleMapLong = dataATMObject.X_Coordinate;
 
   let infowindow = new google.maps.InfoWindow({
     content: `
-        <strong>${googleMapTitle}</strong>
-        <br>${googleMapAddress}<br>
-      `,
+          <strong>${googleMapTitle}</strong>
+          <br>${googleMapAddress}<br>
+        `,
   });
-
-  let map = new google.maps.Map(selectorMapElement, myOptions);
 
   let marker = new google.maps.Marker({
     map: map,
-    position: new google.maps.LatLng(31.665121, 34.585473),
-  });
-
-  let marker2 = new google.maps.Marker({
-    map: map,
-    position: new google.maps.LatLng(38.2847678, -122.9536827),
+    position: new google.maps.LatLng(googleMapLat, googleMapLong),
   });
 
   google.maps.event.addListener(marker, "click", function () {
     infowindow.open(map, marker);
   });
-
-  google.maps.event.addListener(marker2, "click", function () {
-    infowindow.open(map, marker2);
-  });
 }
 
-google.maps.event.addDomListener(window, "load", init_map);
+// let openMapBtn = document.querySelector("#open_map");
